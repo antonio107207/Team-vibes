42bd67f4b8bb4f5eeb8ed1839296284c2e57bf95
Anton Dombrovskii <antonio107207@gmail.com>
2026-06-12 16:27:54 +0200

    improvments

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,3 +1,4 @@
 class Users::SessionsController < Devise::SessionsController
   skip_before_action :authenticate_user!, only: [:new, :create]
+  layout -> { action_name == "new" ? false : "application" }
 end
